c41202eea4fad2d90b2f289e9262ca0d69a2620f
yodah <None>
2025-02-05 20:22:04 +0100

    refactor: Update raw InitVideo

--- elf/kernel/common/kmachine.cpp
+++ elf/kernel/common/kmachine.cpp
@@ -83,13 +83,7 @@ void InitVideo() {
   bool bVar1;
   int iVar2;
   undefined auStack_b0 [96];
-  ulong local_50;
-  ulong local_48;
-  ulong local_40;
-  ulong local_38;
-  undefined local_30;
-  undefined local_2f;
-  undefined local_2e;
+  DisplayEnv display_W;
   
   sceGsResetGraph(0,1,3,0);
   sceGsSetDefLoadImage(auStack_b0,0x2c00,8,0,0,0,0x200,0xe0);
@@ -98,27 +92,27 @@ void InitVideo() {
   sceGsSetDefLoadImage(auStack_b0,0x2c00,8,0,0,0xe0,0x200,0xe0);
   FlushCache(0);
   sceGsExecLoadImage(auStack_b0,0x1070000);
-  memset(&local_50,0,0x28);
-  local_48 = local_48 & 0xfffffffffffffffd | 1;
-  local_50 = local_50 & 0xfffffffffffffffd | 0x60;
-  local_40 = local_40 & 0xffc00000fff00000 | 0x1160;
+  memset(&display_W,0,0x28);
+  display_W.gs_pmode2 = display_W.gs_pmode2 & 0xfffffffffffffffd | 1;
+  display_W.gs_pmode = display_W.gs_pmode & 0xfffffffffffffffd | 0x60;
+  display_W.gs_display_fb = display_W.gs_display_fb & 0xffc00000fff00000 | 0x1160;
   if (false) {
     trap(7);
   }
-  local_38 = local_38 & 0xff800000e0000000 | 0x1bf9ff0204a28c;
-  local_30 = 0;
-  local_2f = 0;
-  local_2e = 0;
+  display_W.gs_display = display_W.gs_display & 0xff800000e0000000 | 0x1bf9ff0204a28c;
+  display_W.gs_bgcolor._0_1_ = 0;
+  display_W.gs_bgcolor._1_1_ = 0;
+  display_W.gs_bgcolor._2_1_ = 0;
   sceGsSyncV(0);
-  sceGsPutDispEnv(&local_50);
+  sceGsPutDispEnv(&display_W);
   iVar2 = 4;
   do {
     sceGsSyncV(0);
     bVar1 = -1 < iVar2;
     iVar2 = iVar2 + -1;
   } while (bVar1);
-  local_50 = local_50 | 2;
-  sceGsPutDispEnv(&local_50);
+  display_W.gs_pmode = display_W.gs_pmode | 2;
+  sceGsPutDispEnv(&display_W);
   return;
 }
 
